f96087c772b4f0f2aadc4a6d7315ed006e707cc8
Hyun-jong Kim <crazyknell913@gmail.com>
2024-05-30 14:31:58 +0900

    edit search.py

--- search.py
+++ search.py
@@ -1,22 +1,37 @@
 import streamlit as st
+import pandas as pd
+from utils import load_data
+
+# 데이터 로드
+df = load_data()
 
 
 def run_search():
-    # 선수 데이터 (추후 API로 만든 데이터프레임 형태로 구현필요)
-    players_data = {
-        "Mike Trout": {"타율": 0.304, "홈런": 45, "타점": 104},
-        "Aaron Judge": {"타율": 0.287, "홈런": 52, "타점": 119},
-        "Mookie Betts": {"타율": 0.319, "홈런": 32, "타점": 80},
-        "Bryce Harper": {"타율": 0.276, "홈런": 35, "타점": 114},
-        "Freddie Freeman": {"타율": 0.310, "홈런": 31, "타점": 102},
-        "Fernando Tatis Jr.": {"타율": 0.285, "홈런": 42, "타점": 97},
-    }
+    # 데이터프레임을 사용하여 선수 데이터 구성
+    players_data = df.groupby(['PlayerName', 'Season']).agg({
+        'GamesPlayed': 'sum',
+        'BattingAverage': 'mean',
+        'AtBats': 'sum',
+        'Runs': 'sum',
+        'Hits': 'sum',
+        'HomeRuns': 'sum',
+        'RBIs': 'sum',
+        'StolenBases': 'sum',
+        'OnBasePercentage': 'mean',
+        'SluggingPercentage': 'mean',
+        'Walks': 'sum',
+        'StrikeOuts': 'sum',
+        'OPS': 'mean'
+    }).reset_index()
 
     # 선수 이름 리스트
-    player_names = list(players_data.keys())
+    player_names = players_data['PlayerName'].unique()
 
     # 제목
     st.title("MLB 선수 기록 조회 및 예측 서비스")
+
+    # 시즌 선택
+    season = st.selectbox("시즌을 선택하세요:", options=sorted(df['Season'].unique()))
 
     # 검색창
     if 'search_query' not in st.session_state:
@@ -24,6 +39,17 @@ def run_search():
 
     search_query = st.text_input(
         "선수 이름을 입력하세요(영문):", st.session_state.search_query, key="search")
+
+    # 선택한 시즌의 모든 선수 데이터 프레임 표시
+    season_data = players_data[players_data['Season']
+                               == season].reset_index(drop=True)
+    season_data_styled = season_data.style.format({"Season": "{:.0f}",
+                                                   'BattingAverage': '{:.3f}',
+                                                   'OnBasePercentage': '{:.3f}',
+                                                   'SluggingPercentage': '{:.3f}',
+                                                   'OPS': '{:.3f}'})
+    st.subheader(f"{season} 시즌 전체 선수 기록")
+    st.dataframe(season_data_styled, height=600)
 
     # 검색어와 유사한 이름 찾기
     if search_query:
@@ -37,10 +63,22 @@ def run_search():
                     st.experimental_rerun()
 
     # 검색 결과 처리
-    if st.session_state.search_query and st.session_state.search_query in players_data:
-        st.subheader(f"{st.session_state.search_query}의 기록")
-        player_stats = players_data[st.session_state.search_query]
-        for stat, value in player_stats.items():
-            st.write(f"{stat}: {value}")
+    if st.session_state.search_query:
+        filtered_data = players_data[(players_data['PlayerName'] == st.session_state.search_query) & (
+            players_data['Season'] == season)].reset_index(drop=True)
+        if not filtered_data.empty:
+            filtered_data_styled = filtered_data.style.format({"Season": "{:.0f}",
+                                                               'BattingAverage': '{:.3f}',
+                                                               'OnBasePercentage': '{:.3f}',
+                                                               'SluggingPercentage': '{:.3f}'})  # 천 단위 구분기호 제거
+            st.subheader(f"{st.session_state.search_query}의 {season} 시즌 기록")
+            # 데이터 프레임 전체를 인덱스 숨김 상태로 표시
+            st.dataframe(filtered_data_styled, height=400)
+        else:
+            st.write(f"{season} 시즌에 대한 해당 선수의 기록을 찾을 수 없습니다.")
     elif search_query:
         st.write("해당 선수의 기록을 찾을 수 없습니다.")
+
+
+if __name__ == "__main__":
+    run_search()
